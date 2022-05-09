1aeb5439a128b97784fce98c479c17df975eb783
Lacey <75631477+LaceyPham@users.noreply.github.com>
2022-05-09 13:01:53 +0700

    Update description

--- weather-app/src/WeatherTemperature.js
+++ weather-app/src/WeatherTemperature.js
@@ -15,9 +15,7 @@ export default function WeatherTemperature(props) {
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">
-          {Math.round(props.celsius)}
-        </span>
+        <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
           °C |
           <a href="/" onClick={showFahrenheit}>
